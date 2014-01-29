57018eea91452de24cf35c331f067a9e352e7900
willedwards <will.edwards@me.com>
2014-01-29 21:09:32 +0000

    removed redundant EMAIL_ID

--- src/main/java/com/google/devrel/samples/ttt/spi/Ids.java
+++ src/main/java/com/google/devrel/samples/ttt/spi/Ids.java
@@ -23,6 +23,4 @@ public class Ids {
   public static final String ANDROID_CLIENT_ID = "android_123";
   public static final String IOS_CLIENT_ID = "ios_123";
   public static final String ANDROID_AUDIENCE = WEB_CLIENT_ID;
-
-  //public static final String EMAIL_SCOPE = "https://www.googleapis.com/auth/userinfo.email";
 }
